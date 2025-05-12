2059e9ffff715294bb91b2051112a1b69a8887ea
B1129005 <113523539+zchsu@users.noreply.github.com>
2025-05-12 23:52:11 +0800

    commit

--- app.py
+++ app.py
@@ -25,6 +25,32 @@ mysql = MySQL(app)
 def index():
     return "Line Bot Server is running!"
 
+@app.route("/", methods=['POST'])
+def linebot():
+    body = request.get_data(as_text=True)  # 取得收到的訊息內容
+    try:
+        json_data = json.loads(body)  # json 格式化訊息內容
+        access_token = os.getenv('LINE_BOT_ACCESS_TOKEN')
+        secret = os.getenv('LINE_BOT_SECRET')
+        line_bot_api = MessagingApi(channel_access_token=access_token)  # 使用新版 MessagingApi
+        handler = WebhookHandler(channel_secret=secret)  # 使用新版 WebhookHandler
+        signature = request.headers['X-Line-Signature']  # 加入回傳的 headers
+        handler.handle(body, signature)  # 綁定訊息回傳的相關資訊
+        tk = json_data['events'][0]['replyToken']  # 取得回傳訊息的 Token
+        type = json_data['events'][0]['message']['type']  # 取得 LINE 收到的訊息類型
+        if type == 'text':
+            msg = json_data['events'][0]['message']['text']  # 取得 LINE 收到的文字訊息
+            print(msg)  # 印出內容
+            reply = msg
+        else:
+            reply = '你傳的不是文字呦～'
+        print(reply)
+        line_bot_api.reply_message(tk, TextSendMessage(reply))  # 回傳訊息
+    except Exception as e:
+        print(f"Error: {e}")  # 如果發生錯誤，印出錯誤訊息
+    return 'OK'  # 驗證 Webhook 使用，不能省略
+
+
 # LINE User API
 @app.route('/line/user', methods=['POST'])
 def create_line_user():
