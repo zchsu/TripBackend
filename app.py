16950b03a56d2caa31216b65125233845b3e648d
B1129005 <113523539+zchsu@users.noreply.github.com>
2025-05-13 02:16:45 +0800

    commit

--- app.py
+++ app.py
@@ -15,10 +15,10 @@ app = Flask(__name__)
 CORS(app)
 
 # MySQL 配置
-app.config['MYSQL_HOST'] = os.getenv('MYSQL_HOST', 'localhost')
-app.config['MYSQL_USER'] = os.getenv('MYSQL_USER', 'root')
-app.config['MYSQL_PASSWORD'] = os.getenv('MYSQL_PASSWORD', '')
-app.config['MYSQL_DB'] = os.getenv('MYSQL_DB', 'tripmate')
+app.config['MYSQL_HOST'] = os.getenv('MYSQL_HOST')
+app.config['MYSQL_USER'] = os.getenv('MYSQL_USER')
+app.config['MYSQL_PASSWORD'] = os.getenv('MYSQL_PASSWORD')
+app.config['MYSQL_DB'] = os.getenv('MYSQL_DB')
 mysql = MySQL(app)
 
 @app.route("/", methods=['GET'])
