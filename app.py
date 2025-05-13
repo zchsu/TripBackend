74012fc829eb3787338d3a2aead5f76e9b292df2
B1129005 <113523539+zchsu@users.noreply.github.com>
2025-05-14 00:55:48 +0800

    commit

--- app.py
+++ app.py
@@ -353,15 +353,14 @@ def search_lockers():
             # 啟動瀏覽器
             browser = p.chromium.launch(
                 headless=True,
-                executable_path=os.getenv('PLAYWRIGHT_BROWSERS_PATH', None),
                 args=[
                     '--no-sandbox',
                     '--disable-setuid-sandbox',
                     '--disable-dev-shm-usage',
                     '--disable-accelerated-2d-canvas',
                     '--disable-gpu',
-                    '--disable-software-rasterizer',
-                    '--single-process'
+                    '--single-process',
+                    '--no-zygote'
                 ]
             )
             context = browser.new_context()
--- build.sh
+++ build.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 python -m pip install --upgrade pip
 pip install -r requirements.txt
-PLAYWRIGHT_BROWSERS_PATH="$PWD/pw-browsers" python -m playwright install chromium
-PLAYWRIGHT_BROWSERS_PATH="$PWD/pw-browsers" python -m playwright install-deps
+python -m playwright install --with-deps chromium
